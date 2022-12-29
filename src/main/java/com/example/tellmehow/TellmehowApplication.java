e2c3439b4d6005233bed66e10e773d175aba8ee5
CoToYo <56789038+CoToYo@users.noreply.github.com>
2022-12-29 14:51:59 -0500

    Initial commit

---
+++ src/main/java/com/example/tellmehow/TellmehowApplication.java
@@ -0,0 +1,13 @@
+package com.example.tellmehow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TellmehowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TellmehowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tellmehow/TellmehowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tellmehow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TellmehowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
